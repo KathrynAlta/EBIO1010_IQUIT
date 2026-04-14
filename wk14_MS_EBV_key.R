d54eb93abf9932ec855214760e0c7de349537af2
Your Name <kgannon59401@gmail.com>
2026-04-14 13:24:07 -0600

    teaching prep R conditional probability

--- wk14_MS_EBV_key.R
+++ wk14_MS_EBV_key.R
@@ -1,7 +1,8 @@
-#HW: Use what you learned in the training script as a guide to evaluate whether the probability of being diagnosed with multiple sclerosis (MS) is conditional on infection by the Epstein-Barr virus (EBv) or not.
+#HW: Use what you learned in the training script as a guide to evaluate whether the probability of being diagnosed 
+# with multiple sclerosis (MS) is conditional on infection by the Epstein-Barr virus (EBv) or not.
 
 #Import the data
-MSdat <- read.csv("EBV_MS.csv")
+MSdat <- read.csv("data/EBV_MS.csv")
 head(MSdat)
 
 #Summarize the data in a table
@@ -16,7 +17,9 @@ flipped_data <- t(MS_summary)
 print(flipped_data)
 
 # graph it, also store coords in barx
-barx <- barplot(flipped_data, beside = T, ylab="Number of Individuals", ylim=c(0,60), main = "Epstein-Barr virus and MS", las= 1, col = c("black", "lightgrey"), xlab = "Epstein-Barr infection")
+barx <- barplot(flipped_data, beside = T, ylab="Number of Individuals Diagnosed with MS", 
+        ylim=c(0,60), main = "Epstein-Barr virus and MS", las= 1, col = c("black", "lightgrey"), 
+        xlab = "Epstein-Barr infection")
 
 #Add a legend to the top right of the graph. Make sure the colors correspond accurately to the colors you put in your graph.
 legend("topright", legend = c("No MS","Yes MS"), pch=15, col = c("black", "lightgrey"))
@@ -25,12 +28,15 @@ legend("topright", legend = c("No MS","Yes MS"), pch=15, col = c("black", "light
 chisq.test(flipped_data)
 summary(flipped_data)
 
-# slightly different because chisq.test() uses continuity correction (-0.5 to account for small sample sizes... esp. in 2x2 tables)
+# slightly different because chisq.test() uses continuity correction (-0.5 to account for small sample sizes... 
+# esp. in 2x2 tables)
 
 
-#The barx object shows the x values for each bar (where each bar is located along the x axis). You can use this information to designate where to place your p value text.
+#The barx object shows the x values for each bar (where each bar is located along the x axis). 
+# You can use this information to designate where to place your p value text.
 print(barx)
 #Let's put the text at x = 3.5 and y = 50:
 text(3.5, 50, "Chisq = 16.352 \n p-value = 5.261e-05")
 
-# claim: MS diagnosis seems to be influenced by Epstein-Barr virus infection - absence of infection greatly decreases probability of MS diagnosis.
+# claim: MS diagnosis seems to be influenced by Epstein-Barr virus infection - absence of infection greatly 
+# decreases probability of MS diagnosis.
